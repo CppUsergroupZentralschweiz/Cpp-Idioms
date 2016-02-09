c06aac1906b41e700fe4f7c0624ee8beedddd25e
Michel Estermann <estermann.michel@gmail.com>
2016-02-09 22:06:57 +0100

    Erase Remove Idiom: Solution

--- Idiom_Erase_Remove/idiom.h
+++ Idiom_Erase_Remove/idiom.h
@@ -6,6 +6,16 @@
 template<typename T, class Container>
 void remove_multiples_of(T multiple_of, Container& container)
 {
+  using std::begin;
+  using std::end;
+
+  container.erase(std::remove_if(begin(container),
+                                 end(container),
+                                 [&multiple_of](typename Container::value_type const& elem)
+                                 {
+                                   return elem % multiple_of == 0;
+                                 }),
+                  end(container));
 }
 
 #endif
