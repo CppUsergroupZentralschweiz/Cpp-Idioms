cd12c1a19d5aabf20fea9d88520a3b810d753a32
Michel Estermann <estermann.michel@gmail.com>
2016-02-10 22:11:26 +0100

    Sort: Added implementation.

--- Sort/sort.h
+++ Sort/sort.h
@@ -6,11 +6,17 @@
 template<typename Container>
 void sort_ascending(Container& container)
 {
+  using std::begin;
+  using std::end;
+  std::sort(std::begin(container), std::end(container));
 }
 
 template<typename Container>
 void sort_descending(Container& container)
 {
+  using std::begin;
+  using std::end;
+  std::sort(std::begin(container), std::end(container), std::greater<typename Container::value_type>());
 }
 
 #endif
