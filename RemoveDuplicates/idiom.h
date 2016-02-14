a858fb040a33fef159d71041ded43a1639e74e13
Michel Estermann <estermann.michel@gmail.com>
2016-02-14 19:18:38 +0100

    Added remove duplicates soluction.

--- RemoveDuplicates/idiom.h
+++ RemoveDuplicates/idiom.h
@@ -6,7 +6,10 @@
 template<class Container>
 void sort_and_remove_duplicates(Container& container)
 {
-
+  using std::begin;
+  using std::end;
+  std::sort(begin(container), end(container));
+  container.erase(std::unique(begin(container), end(container)), end(container));
 }
 
 #endif
