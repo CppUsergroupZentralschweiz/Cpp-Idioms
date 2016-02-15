83ddefd6229a11818fe43302b9c005598f277532
Michel Estermann <estermann.michel@gmail.com>
2016-02-15 18:38:09 +0100

    RAII: Solution.

--- RAII/main.cpp
+++ RAII/main.cpp
@@ -1,30 +1,37 @@
 #include <cstdlib>
 #include <cstdio>
+#include <memory>
 
 using namespace std;
 
 int main ()
 {
-    FILE* fp = std::fopen("test.txt", "r");
-    if(!fp) {
-        std::perror("File opening failed");
-        return EXIT_FAILURE;
-    }
+  auto file_open = [](const char* filename, const char* mode) -> FILE* {
+    return std::fopen(filename, mode);
+  };
 
-    int c{};
-    while ((c = std::fgetc(fp)) != EOF) { // standard C I/O file reading loop
-        std::putchar(c);
-    }
+  auto file_deleter=[](FILE* file) {
+      std::puts("Close file\n");
+      std::fclose(file);
+  };
 
-    if (std::ferror(fp))
-    {
-        std::puts("I/O error when reading");
-        return EXIT_FAILURE;
-    }
-    else if (std::feof(fp))
-    {
-        std::puts("End of file reached successfully");
-    }
-    std::fclose(fp);
-    return EXIT_SUCCESS;
+  // Using a unique pointer and custom deleter (lamda)
+  std::unique_ptr<FILE, decltype(file_deleter)> fp{file_open("test.txt", "r"), file_deleter};
+  if(!fp) {
+      std::perror("File opening failed");
+      return EXIT_FAILURE;
+  }
+
+  int c{};
+  while ((c = std::fgetc(fp.get())) != EOF) {
+     std::putchar(c);
+  }
+
+  if (std::ferror(fp.get())) {
+      std::puts("I/O error when reading");
+      return EXIT_FAILURE;
+  } else if (std::feof(fp.get())) {
+      std::puts("End of file reached successfully");
+  }
+  return EXIT_SUCCESS;
 }
